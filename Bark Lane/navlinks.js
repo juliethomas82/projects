adc02a97820a540693f057c3ac346b037fda1f46
Julie Thomas <102959256+juliethomas82@users.noreply.github.com>
2022-07-03 17:43:23 +0100

    Add files via upload

---
+++ Bark Lane/navlinks.js
@@ -0,0 +1,8 @@
+      var navLinks = document.getElementById("navLinks");
+      
+      function showMenu(){
+          navLinks.style.top = "0"; 
+      }
+      function hideMenu(){
+          navLinks.style.top = "-100vh"; 
+      }
